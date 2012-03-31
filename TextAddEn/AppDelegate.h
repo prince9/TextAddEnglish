c847fa47d49193389e5c302d25ea39f54138e387
MAYU <mtsuzaka@rb3.so-net.ne.jp>
2012-04-01 01:45:28 +0900

    Initial Commit

---
+++ TextAddEn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextAddEn
+//
+//  Created by 真有 津坂 on 12/04/01.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TextAddEn/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TextAddEn
+//
+//  Created by 真有 津坂 on 12/04/01.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
